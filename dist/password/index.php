9be4e77ee0b87a00b983131e7784e519d4773f08
Jason Rundell <jason.rundell@gmail.com>
2019-01-16 22:18:52 -0500

    New build process with static files

---
+++ dist/password/index.php
@@ -0,0 +1,18 @@
+<?php
+function randomPassword($length) {
+    $alphabet = "abcdefghijklmnopqrstuwxyzABCDEFGHIJKLMNOPQRSTUWXYZ0123456789!%$#()&^";
+    $pass = array(); //remember to declare $pass as an array
+    $alphaLength = strlen($alphabet) - 1; //put the length -1 in cache
+    for ($i = 0; $i < $length; $i++) {
+        $n = rand(0, $alphaLength);
+        $pass[] = $alphabet[$n];
+    }
+    return implode($pass); //turn the array into a string
+}
+if (isset($_GET['length'])) {
+  echo randomPassword($_GET['length']);
+} else {
+  echo randomPassword(8);
+}
+
+
