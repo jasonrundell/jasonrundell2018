5569e8f96a9dd422114a8634d6ca8d2ac8840837
Jason Rundell <jason.rundell@gmail.com>
2019-01-31 22:00:46 -0500

    AOS now included part of JS build and PurifyCSS added to build

--- src/index.js
+++ src/index.js
@@ -1,10 +1,14 @@
 import 'babel-polyfill';
 import React from 'react';
 import { render } from 'react-dom';
+import AOS from 'aos';
 
 import App from './App';
+
 
 render(
   <App />,
   document.getElementById('root')
 );
+
+AOS.init();
