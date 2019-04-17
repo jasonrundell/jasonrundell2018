2324881514b7a3baa6e5e7f7dc47a72d0f8547e8
Jason Rundell <jason.rundell@gmail.com>
2019-04-17 14:26:06 -0400

    PWA experiment updated

--- src/index.js
+++ src/index.js
@@ -12,3 +12,21 @@ render(
 );
 
 AOS.init();
+
+if ('serviceWorker' in navigator) {
+
+navigator.serviceWorker.register('sw.js').then(registration => {
+
+  // Registration was successful
+
+  console.log('ServiceWorker registration successful with scope: ',    registration.scope);
+
+}).catch(err => {
+
+  // registration failed :(
+
+  console.log('ServiceWorker registration failed: ', err);
+
+});
+
+}
