177649edccf63c95017d1d512d78e524ad059a7a
Jason Rundell <jason.rundell@gmail.com>
2019-05-08 21:01:31 -0400

    Sorting skills alphabetically

--- src/components/Skills.js
+++ src/components/Skills.js
@@ -49,7 +49,21 @@ const skillKeywords = [
   'Accessibility',
   'Adobe Photoshop',
   'Adobe Illustrator',
-]
+];
+
+skillKeywords.sort(function(a, b) {
+  var nameA = a.toUpperCase(); // ignore upper and lowercase
+  var nameB = b.toUpperCase(); // ignore upper and lowercase
+  if (nameA < nameB) {
+    return -1;
+  }
+  if (nameA > nameB) {
+    return 1;
+  }
+
+  // names must be equal
+  return 0;
+});
 
 const Skills = () =>
   <UnorderedList
