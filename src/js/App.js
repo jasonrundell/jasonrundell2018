014fabe549b9235ef7f2a8ea7cd49e7ef56b0871
Jason Rundell <jason.rundell@gmail.com>
2019-06-05 18:08:56 -0400

    New build with theme work started

--- src/js/App.js
+++ src/js/App.js
@@ -3,7 +3,7 @@ import {
   BrowserRouter as Router, Route, Switch, Link,
 } from 'react-router-dom';
 import Home from './Home';
-import '../scss/styles.scss';
+import '../scss/base.scss';
 
 const NoMatch = location => (
   <React.Fragment>
