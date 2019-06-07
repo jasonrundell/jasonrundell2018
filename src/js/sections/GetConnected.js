b6e2877d999868f73c1388b6aeefd26c7a045747
Jason Rundell <jason.rundell@gmail.com>
2019-06-07 14:58:26 -0400

    SCSS/CSS optimizations

--- src/js/sections/GetConnected.js
+++ src/js/sections/GetConnected.js
@@ -5,7 +5,7 @@ const GetConnected = () => (
   <section id="contact" className="section section-contact" data-aos={AOS_CONFIG.type} data-aos-duration={AOS_CONFIG.duration} data-aos-delay={AOS_CONFIG.delay}>
     <div className="contain">
       <h2 className="section-title">Get connected</h2>
-      <ul>
+      <ul className="lists__contact">
         <li>
           <a href="mailto:contact@jasonrundell.com" className="link">contact@jasonrundell.com</a>
         </li>
