67729f9ab63e2989660e791a632f81b87261d189
Jason Rundell <jason.rundell@gmail.com>
2019-06-13 12:57:20 -0400

    Lighthouse build now with props

--- src/js/components/LightHouseBadge.js
+++ src/js/components/LightHouseBadge.js
@@ -2,31 +2,33 @@ import React from 'react';
 import Logo from '../../images/logos/lighthouse.png';
 import './LighthouseBadge.scss';
 
-const LIGHTHOUSE_REPORT_URL = '/files/lighthouse/jasonrundell.com_2019-06-10_17-20-35.html';
+/**
+ * todo: add props to function for the scores, date, and link to report
+ */
 
-const LightHouseBadge = () => (
+const LightHouseBadge = (props) => (
   <div className="LightHouseBadge">
     <div className="row__scores">
       <div className="detail">
-        <div className="score score--pass">96</div>
+        <div className="score score--pass">{props.performanceScore}</div>
         <div className="label">Performance</div>
       </div>
       <div className="detail">
-        <div className="score score--pass">100</div>
+        <div className="score score--pass">{props.accessibilityScore}</div>
         <div className="label">Accessibility</div>
       </div>
       <div className="detail">
-        <div className="score score--pass">93</div>
+        <div className="score score--pass">{props.bestPracticesScore}</div>
         <div className="label">Best Practices</div>
       </div>
       <div className="detail">
-        <div className="score score--pass">100</div>
+        <div className="score score--pass">{props.seoScore}</div>
         <div className="label">SEO</div>
       </div>
     </div>
     <div className="row__report">
       <img src={Logo} className="logo" alt="" />
-      <a href={LIGHTHOUSE_REPORT_URL} className="link">View Lighthouse 2019-06-10 Report</a>
+      <a href={props.reportFileUrl} className="link">View Lighthouse {props.dateText} Report</a>
     </div>
   </div>
 );
