829589a78dbbb21278e58d5b1af706d221d7c159
Jason Rundell <jason.rundell@gmail.com>
2019-10-28 23:54:58 -0400

    Another attempt to fix dependency error

--- src/js/components/LightHouseBadge/index.js
+++ src/js/components/LightHouseBadge/index.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import PropTypes from 'prop-types'
 import Logo from './lighthouse.png'
-import styles from '../LightHouseBadge/LightHouseBadge.styles'
+import styles from './LightHouseBadge.styles.js'
 
 const LightHouseBadge = ({
   performanceScore,
