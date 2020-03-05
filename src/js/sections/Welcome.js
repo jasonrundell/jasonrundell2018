80eec5e387ae669852d93c23262257d27b0958ef
Jason Rundell <jason.rundell@gmail.com>
2020-03-05 10:51:29 -0500

    Updated intro

--- src/js/sections/Welcome.js
+++ src/js/sections/Welcome.js
@@ -24,10 +24,10 @@ const Welcome = () => (
         data-aos-duration={AOS_CONFIG.duration}
         data-aos-delay={AOS_CONFIG.delay}
       >
-        Yo! I&apos;m a developer who loves learning and using the latest in
-        front end web development. My skill set includes experience with React,
-        Gulp, PHP, MySQL, SASS, CSS, HTML, JavaScript, and Amazon Web Services
-        (just to name a few).
+        Hello. I'm a developer who loves learning and using the latest in front
+        end web development. My skill set includes experience with React,
+        Gatsby, AEM, Contentful, WordPress, Git, Gulp, Grunt, Parcel, PHP,
+        MySQL, SASS, CSS, HTML, JavaScript, and Amazon Web Services.
       </p>
       <p
         data-aos={AOS_CONFIG.type}
@@ -36,13 +36,14 @@ const Welcome = () => (
       >
         My love of building web experiences started in my high school’s library
         in 1997 with GeoCities and it’s been rewarding ever since! As you can
-        imagine, with over 20 years of love for HTML, I have been a part of a
-        wide variety of web projects: from iframes, to Flash, jQuery Mobile, and
-        now ‘isomorphic JavaScript applications’. Whatever the latest trend is,
-        I’m either deeply involved or experimenting with it in my spare time and
-        imagining what’s next. My skills and experiences are deep, wide-ranging,
-        and I am always seeking new best practices and methodologies. I embrace
-        change, crave challenge, and love technology!
+        imagine, with over 20 years of love for the web, I have been a part of a
+        wide variety of web projects: from iframes, to Flash, WordPress
+        multisites, jQuery Mobile, custom CMS applications, a Facebook contest
+        platform, React design systems, and now JAMstack with Gatsby and
+        headless CMSs. Whatever the latest trend is, I’m either deeply involved
+        or experimenting with it in my spare time. My skills and experiences are
+        deep, wide-ranging, and I am always seeking new best practices and
+        methodologies. I embrace change, crave challenge, and love technology!
       </p>
     </div>
   </section>
